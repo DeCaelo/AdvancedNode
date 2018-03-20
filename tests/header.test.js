00b180db9a12b66b1a2192ce817ac09229ae6096
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-03-20 11:17:40 +0100

    74. Extracting Page Content

--- tests/header.test.js
+++ tests/header.test.js
@@ -9,4 +9,9 @@ test('Adds two numbers', () => {
 test('We can launch a browser', async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  await page.goto('localhost:3000');
+
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
 });
