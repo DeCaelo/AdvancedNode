a1a7e0c0762258de810489bc8bcfe18f75187e08
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-03-21 15:13:20 +0100

    90. Session Factory

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
 
 let browser, page;
 
@@ -26,23 +27,10 @@ test('clicking login starts oauth flow', async () => {
   expect(url).toMatch(/accounts\.google\.com/);
 });
 
-test.only('When signed in, shows logout button', async () => {
+test('When signed in, shows logout button', async () => {
   const id = '5aab9cc16f65143708b334e2';
 
-  const Buffer = require('safe-buffer').Buffer;
-  const sessionObject = {
-    passport: {
-      user: id,
-    },
-  };
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
-    'base64',
-  );
-
-  const Keygrip = require('keygrip');
-  const keys = require('../config/keys');
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign('session=' + sessionString);
+  const { session, sig } = sessionFactory();
 
   await page.setCookie({ name: 'session', value: sessionString });
   await page.setCookie({ name: 'session.sig', value: sig });
