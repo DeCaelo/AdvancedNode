2ea8dc9068a26fda896099e2a04bcfd3ff31d1dc
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-03-29 09:14:09 +0200

    need create amazon S3 account => 150. Creating S3 Buckest

--- config/dev.js
+++ config/dev.js
@@ -5,4 +5,6 @@ module.exports = {
   mongoURI: 'mongodb://decaelo:password@ds111078.mlab.com:11078/blog_dev',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAIWSXFHRM7F6Z3NWQ',
+  secretAccessKey: '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',
 };
